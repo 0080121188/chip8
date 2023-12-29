e8be41d76057dc9147f7b847e717b3bcd61e3ad6
max <maxr.0307@gmail.com>
2023-12-29 20:25:31 +0100

    constexpr -> inline constexpr

--- chip8.h
+++ chip8.h
@@ -1,8 +1,8 @@
 #pragma once
 
 namespace chip8 {
-constexpr int memory_capacity{4096};
-constexpr int register_amount{16};
-constexpr int display_width{64};
-constexpr int display_height{32};
+inline constexpr int memory_capacity{4096};
+inline constexpr int register_amount{16};
+inline constexpr int display_width{64};
+inline constexpr int display_height{32};
 } // namespace chip8
